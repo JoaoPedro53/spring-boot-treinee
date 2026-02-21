c803b432aa3374afb621077f0d3321b764567028
JoaoPedro53 <jpsv32@gmail.com>
2026-02-21 01:11:05 -0300

    add responseEntity

--- anime-service/src/main/java/developer/jota/controller/ProducerController.java
+++ anime-service/src/main/java/developer/jota/controller/ProducerController.java
@@ -4,7 +4,9 @@ import developer.jota.domain.Anime;
 import developer.jota.domain.Producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,10 +37,13 @@ public class ProducerController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-key=1234")
-    public Producer save(@RequestBody Producer producer, @RequestHeader HttpHeaders headers){
+    public ResponseEntity<Producer> save(@RequestBody Producer producer, @RequestHeader HttpHeaders headers){
         producer.setId(ThreadLocalRandom.current().nextLong(100_000));
         Producer.getProducers().add(producer);
-        return producer;
+
+        var responseHeaders = new HttpHeaders();
+        responseHeaders.add("Authorization", "MY_KEY");
+        return ResponseEntity.status(HttpStatus.CREATED).headers(responseHeaders).body(producer);
     }
 
 }
