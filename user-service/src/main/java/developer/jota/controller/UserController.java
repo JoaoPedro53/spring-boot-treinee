1711b339c7a8d2cd00d39be4ecf73b8691501359
JoaoPedro53 <jpsv32@gmail.com>
2026-04-27 14:16:11 -0300

    refactor(UserController): Altered to '@RequiredArgsConstructor. Added 'UserMapper' dependency. Added 'mapper.toListGetResponse' in listAll(). Altered return 'List<User>' -> 'List<UserGetResponse>'.

--- user-service/src/main/java/developer/jota/controller/UserController.java
+++ user-service/src/main/java/developer/jota/controller/UserController.java
@@ -1,8 +1,9 @@
 package developer.jota.controller;
 
-import developer.jota.models.User;
+import developer.jota.mapper.UserMapper;
+import developer.jota.response.UserGetResponse;
 import developer.jota.service.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/users")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserController {
     private final UserService service;
+    private final UserMapper mapper;
 
     @GetMapping
-    public ResponseEntity<List<User>> listAll() {
-        return ResponseEntity.ok(service.listAll());
+    public ResponseEntity<List<UserGetResponse>> listAll() {
+        var listUserGetResponse = mapper.toListUserGetResponse(service.listAll());
+        return ResponseEntity.ok(listUserGetResponse);
     }
 }
