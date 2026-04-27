f285c3f90a04b4bd47a5caa36ce590dc6d1ce516
JoaoPedro53 <jpsv32@gmail.com>
2026-04-27 14:03:59 -0300

    feat: Created UserMappper and UserGetResponse, for DTO pattern.

--- user-service/src/main/java/developer/jota/mapper/UserMapper.java
+++ user-service/src/main/java/developer/jota/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package developer.jota.mapper;
 
+import developer.jota.models.User;
+import developer.jota.response.UserGetResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
+
+    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+    UserGetResponse toUserGetResponse(User user);
+
+    List<UserGetResponse> toListUserGetResponse(List<User> users);
 }
