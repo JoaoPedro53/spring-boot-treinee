be698743f521e300a77ee563fae375fda92c6500
JoaoPedro53 <jpsv32@gmail.com>
2026-04-27 21:17:25 -0300

    feat: created PostResponse and PostRequest to method save{UserController}.

---
+++ user-service/src/main/java/developer/jota/response/UserPostResponse.java
@@ -0,0 +1,17 @@
+package developer.jota.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserPostRequest {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
