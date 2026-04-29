8157b244b4fb4ff1ab2389987000f380f15b42b3
JoaoPedro53 <jpsv32@gmail.com>
2026-04-28 21:44:27 -0300

    feat(UserService): created delete and findByIdAndReturnUserOrThrowNotFound methods.

--- user-service/src/main/java/developer/jota/service/UserService.java
+++ user-service/src/main/java/developer/jota/service/UserService.java
@@ -3,7 +3,9 @@ package developer.jota.service;
 import developer.jota.models.User;
 import developer.jota.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,7 +18,17 @@ public class UserService {
         return repository.listAll();
     }
 
+    public User findByIdAndReturnUserOrThrowNotFound(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not Found"));
+    }
+
     public User save(User user){
         return repository.save(user);
+    }
+
+    public void delete(Long id){
+        var user = findByIdAndReturnUserOrThrowNotFound(id);
+        repository.delete(user);
     }
 }
