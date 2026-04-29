89d112aacc83450bb9258226627a6538fc54bd30
JoaoPedro53 <jpsv32@gmail.com>
2026-04-29 11:56:35 -0300

    feat(UserController): created 'update(PUT)' method.

--- user-service/src/main/java/developer/jota/controller/UserController.java
+++ user-service/src/main/java/developer/jota/controller/UserController.java
@@ -3,11 +3,12 @@ package developer.jota.controller;
 import developer.jota.mapper.UserMapper;
 import developer.jota.models.User;
 import developer.jota.request.UserPostRequest;
+import developer.jota.request.UserPutRequest;
 import developer.jota.response.UserGetResponse;
 import developer.jota.response.UserPostResponse;
+import developer.jota.response.UserPutResponse;
 import developer.jota.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,5 +42,15 @@ public class UserController {
 
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<UserPutResponse> update(@RequestBody UserPutRequest putRequest) {
+
+        var request = mapper.toUser(putRequest);
+        var user = service.update(request);
+        var response = mapper.toUserPutResponse(user);
+
+        return ResponseEntity.ok(response);
     }
 }
