55fc3a60c3f42ef8568739032b30232c713a86c6
JoaoPedro53 <jpsv32@gmail.com>
2026-04-29 11:53:26 -0300

    feat(PutResponse, PutRequest): added implementation.

--- user-service/src/main/java/developer/jota/response/UserPutResponse.java
+++ user-service/src/main/java/developer/jota/response/UserPutResponse.java
@@ -1,4 +1,17 @@
 package developer.jota.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class UserPutResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
