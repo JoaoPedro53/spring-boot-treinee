685c861fab3fa20d69d562cad85a33e17a418044
JoaoPedro53 <jpsv32@gmail.com>
2026-04-28 21:46:48 -0300

    feat(UserRepository): created delete and findById methods.

--- user-service/src/main/java/developer/jota/repository/UserRepository.java
+++ user-service/src/main/java/developer/jota/repository/UserRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,9 +16,17 @@ public class UserRepository {
         return repositoryData.getUSERS();
     }
 
+    public Optional<User> findById(Long id) {
+        return repositoryData.getUSERS().stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
     public User save(User user){
         repositoryData.getUSERS().add(user);
         return user;
+    }
+
+    public void delete(User user){
+        repositoryData.getUSERS().remove(user);
     }
 
 }
