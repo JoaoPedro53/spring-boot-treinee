17943bd94fa05620c83d5c5b36fa13810e964027
JoaoPedro53 <jpsv32@gmail.com>
2026-04-28 21:12:30 -0300

    feat(UserController): created save(POST) method.

--- user-service/src/main/java/developer/jota/controller/UserController.java
+++ user-service/src/main/java/developer/jota/controller/UserController.java
@@ -1,13 +1,16 @@
 package developer.jota.controller;
 
 import developer.jota.mapper.UserMapper;
+import developer.jota.models.User;
+import developer.jota.request.UserPostRequest;
 import developer.jota.response.UserGetResponse;
+import developer.jota.response.UserPostResponse;
 import developer.jota.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +25,14 @@ public class UserController {
     public ResponseEntity<List<UserGetResponse>> listAll() {
         var listUserGetResponse = mapper.toListUserGetResponse(service.listAll());
         return ResponseEntity.ok(listUserGetResponse);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserPostResponse> save(@RequestBody UserPostRequest postRequest) {
+        var userToSave = mapper.toUser(postRequest);
+        var user = service.save(userToSave);
+        var response = mapper.toUserPostResponse(user);
+
+        return ResponseEntity.ok(response);
     }
 }
